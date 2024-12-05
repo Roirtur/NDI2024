b58d5a93475650f29cc9d563f4524e2fdfbd86c5
Roirtur <arthurmac33@gmail.com>
2024-12-05 21:35:45 +0100

    fix compile crash

--- ndi-front/src/index.tsx
+++ ndi-front/src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <ThemeContextProvider>
-      <Router>
+      <Router basename="/NDI2024">
         <Box
           sx={{
             display: 'flex',
