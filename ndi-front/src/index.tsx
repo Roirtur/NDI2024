1063008399d62bd2dc67f4cba06467004fc21612
Roirtur <arthurmac33@gmail.com>
2024-12-05 22:23:57 +0100

    path fix

--- ndi-front/src/index.tsx
+++ ndi-front/src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <ThemeContextProvider>
-      <Router basename="/NDI2024">
+      <Router>
         <Box
           sx={{
             display: 'flex',
